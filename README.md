ac580425c78fa1b5d15440363e4c736e70ad2a67
yagita <takashi.yagita@ibm.com>
2019-10-17 14:02:00 +0900

    first commit

--- README.md
+++ README.md
@@ -1,23 +1,14 @@
 # AWSServiceHealthNotification
+Notify the health status of "AWS Transfer for SFTP"  
 
-AWS Personal Health DashboardはAWS Transfer for SFTPをサポートしていない。
-そのため、そのサービスのステータスをAWS Service Health Dashboardから取得しメール通知する。
-
-動作仕様
-1. AWS CloudWatch Schedule Eventで起動されたAWS Lambda functionがAWS Personal Health Dashboardから情報(xml)を取得する。
-
-2｡ AWS Lambda functionは取得した情報(xml)から通知すべき情報があれば、AWS SNSを使ってmail通知する。
+The AWS Personal Health Dashboard does not support AWS Transfer for SFTP.  
+Therefore, the status of the service is obtained from the AWS Service Health Dashboard and notified by email.  
 
 
-Notify the health status of "AWS Transfer for SFTP"
+__Operating specifications__
+1. AWS Lambda function started by AWS CloudWatch Schedule Event gets information (xml) from AWS Service Health Dashboard.
+2. AWS Lambda function sends mail notification using AWS SNS if there is information(item) to be notified in the acquired information (rss).  
 
-AWS Transfer for SFTP is not supported by the AWS Personal Health Dashboard.
-Get the status of the service from the AWS Service Health Dashboard and send an email notification.
-
-Operating specifications
-1. AWS Lambda function started by AWS CloudWatch Schedule Event gets information (xml) from AWS Personal Health Dashboard.
-
-2. AWS Lambda function sends mail notification using AWS SNS if there is information to be notified from the acquired information (xml).
 
 ![](./images/AWSServiceHealthNotification.svg)  
 
@@ -32,6 +23,45 @@ Operating specifications
 
 These are the deployment steps until the full implementation is complete.:
 
+Locally, run following commands. 
+
+1. Set variables
+
+UPLOADBUCKETNAME:  
+Create S3Bucket to upload Lambda Function in advance.  
+Set BucketName to the following UPLOADBUCKETNAME.  
+
+MAILADDRESS:  
+Destination address for notifying Service Status.
+
+
+```bash
+$ PROJECTNAME=opstools
+$ ROLENAME=healthcheck
+$ UPLOADBUCKETNAME=xxxxxxxxxxxx
+$ MAILADDRESS=xxxxx@xxx.xxx
+
+```
+
+2. Upload local artifacts  
+```bash
+$ aws cloudformation package \
+    --template-file health_check.yaml \
+    --s3-bucket $UPLOADBUCKETNAME \
+    --output-template-file packaged.yml
+```
+
+3. Deploys the specified AWS CloudFormation template
+```bash
+$ aws cloudformation deploy \
+    --stack-name $PROJECTNAME-$ROLENAME \
+    --region ap-northeast-1 \
+    --template-file packaged.yml \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --output text \
+    --parameter-overrides \
+        NortificationMail=$MAILADDRESS
+```
 
 
 ### RSS Sample from [AWS Service Health Dashboard](https://status.aws.amazon.com/)
---
+++ lambda/lambda_function.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+import os
+import urllib.request
+import xml.etree.ElementTree as et
+from datetime import datetime, timezone
+import logging
+import boto3
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+sns = boto3.client('sns')
+
+services = {
+    "ap-northeast-1": {
+        "Transfer for SFTP": 'https://status.aws.amazon.com/rss/transfer-ap-northeast-1.rss',
+        "EC2": "https://status.aws.amazon.com/rss/ec2-ap-northeast-1.rss",
+        "S3": "https://status.aws.amazon.com/rss/s3-ap-northeast-1.rss",
+        "Lambda": "https://status.aws.amazon.com/rss/lambda-ap-northeast-1.rss",
+        "IoT": "https://status.aws.amazon.com/rss/awsiot-ap-northeast-1.rss"
+    },
+    "us-east-1": {
+        "EC2": 'https://status.aws.amazon.com/rss/ec2-us-east-1.rss'
+    },
+    "global": {
+        "Console": "https://status.aws.amazon.com/rss/management-console.rss",
+        "Route53": "https://status.aws.amazon.com/rss/route53.rss"
+    }
+}
+
+SNS_TOPIC_ARN = os.environ['SNS_TOPIC_ARN']
+
+
+def pub_date_to_utc(pub_date):
+    # e.g. pub_date: Thu, 22 Aug 2019 23:40:01 PDT
+    logger.info('pub_date: {}'.format(pub_date))
+    elements = pub_date.split(' ')
+
+    if elements[-1] == 'PDT':
+        _date = elements[1:-1]
+        _date.append('-0700')
+        _pub_date = ' '.join(_date)
+        utc_time = datetime.strptime(_pub_date, '%d %b %Y %H:%M:%S %z').\
+            astimezone(timezone.utc)
+        return utc_time
+    elif elements[-1] == 'PCT':
+        _date = elements[1:-1]
+        _date.append('-0800')
+        _pub_date = ' '.join(_date)
+        utc_time = datetime.strptime(_pub_date, '%d %b %Y %H:%M:%S %z').\
+            astimezone(timezone.utc)
+        return utc_time
+    else:
+        raise ValueError('datetime format error: {}'.format(pub_date))
+
+
+def rss_read(url):
+    """
+    return: item(dict) list
+    item: {
+       'pub_date':
+          datetime.datetime(2019,8,23,11,18,35,tzinfo=datetime.timezone.utc),
+       'description':
+          'The majority of impaired EC2 instances and EBS...'
+    }
+    """
+    root = et.fromstring(urllib.request.urlopen(url).read())
+
+    items = list()
+    for item in root.iter('item'):
+        pub_date = pub_date_to_utc(item[2].text)
+        description = item[4].text
+        items.append(dict(pub_date=pub_date, description=description))
+
+    logger.info('item list: {}'.format(items))
+    return items
+
+
+def message_format(item, aws_service, region):
+    message = 'AWS Service Error!!\n'\
+              '{} in {}\n'\
+              'UTC:{}\n\nMESSAGE:\n{}'\
+        .format(
+            aws_service,
+            region,
+            item['pub_date'].strftime('%Y/%m/%d %H:%M:%S'),
+            item['description'])
+
+    logger.info(message)
+    return message
+
+
+def alert_service_status(items, aws_service, region):
+    # CloudWatch Logsに送る
+    # CloudWatch Scheduled Events 5分間隔
+    # 5分 × 2回分 = 600秒
+    # 直近10分以内のItemを通知する
+    logger.info('xml: {}'.format(items))
+
+    for item in items:
+        delta = datetime.now(timezone.utc) - item['pub_date']
+
+        # if delta.total_seconds() <= 60 * 5 * 2 * 10000000:  # for test
+        if delta.total_seconds() <= 60 * 5 * 2:
+            message = message_format(item, aws_service, region)
+            sns.publish(
+                TopicArn=SNS_TOPIC_ARN,
+                Message=message,
+                Subject='Alert! AWS Service Error. {} in {}'.format(
+                    aws_service, region))
+            logger.info('Message published: {} in {}'.format(
+                aws_service, region))
+
+
+def lambda_handler(event, context):
+    logger.info('event: {}'.format(event))
+    try:
+        region = 'ap-northeast-1'
+        aws_service = 'Transfer for SFTP'
+        items = rss_read(services.get(region).get(aws_service))
+        if len(items):
+            alert_service_status(items, aws_service, region)
+        else:
+            logger.info('Normal Operation: AWS Service {} in {}'.format(
+                aws_service, region))
+
+    except Exception as e:
+        logger.error('Error: {}'.format(str(e)))
